59f7db49e16818398c5be5a6f987d7bf61df117c
caroldf07 <carolina.df@outlook.com>
2021-05-19 23:49:13 -0300

    testes unitários de criar e autenticar o usuário

---
+++ src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
@@ -0,0 +1,35 @@
+import { InMemoryUsersRepository } from "./../../repositories/in-memory/InMemoryUsersRepository";
+import { CreateUserUseCase } from "./CreateUserUseCase";
+import { CreateUserError } from "./CreateUserError";
+
+let createUserUseCase: CreateUserUseCase;
+let userRepositoryInMemory: InMemoryUsersRepository;
+describe("Create User", () => {
+  beforeEach(() => {
+    userRepositoryInMemory = new InMemoryUsersRepository();
+    createUserUseCase = new CreateUserUseCase(userRepositoryInMemory);
+  });
+
+  it("deve criar um novo usuário", async () => {
+    await createUserUseCase.execute({
+      name: "name",
+      email: "email@email.com",
+      password: "password",
+    });
+  });
+
+  it("não dever criar um novo usuário quando ele já existe", async () => {
+    expect(async () => {
+      await createUserUseCase.execute({
+        name: "name",
+        email: "email@email.com",
+        password: "password",
+      });
+      await createUserUseCase.execute({
+        name: "name",
+        email: "email@email.com",
+        password: "password",
+      });
+    }).rejects.toBeInstanceOf(CreateUserError);
+  });
+});
