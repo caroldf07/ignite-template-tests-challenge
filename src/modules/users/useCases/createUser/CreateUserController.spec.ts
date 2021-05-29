c2398b5d8868b5ffbaf5d8732f5cb39ab8096ec7
caroldf07 <carolina.df@outlook.com>
2021-05-29 13:02:06 -0300

    Concluído testes de integração

--- src/modules/users/useCases/createUser/CreateUserController.spec.ts
+++ src/modules/users/useCases/createUser/CreateUserController.spec.ts
@@ -17,10 +17,31 @@ describe("Create User Controller", () => {
     await connection.close();
   });
   it("deve retornar 201 com resposta vazia", async () => {
+    //cenário
+
+    //ação
     const response = await request(app)
       .post("/api/v1/users")
       .send({ name: "teste", email: "email@email.com.br", password: "senha" });
 
+    //validação
     expect(response.status).toBe(201);
+    expect(response.body).toStrictEqual({});
+  });
+
+  it("deve retornar 400 quando o usuário já existir", async () => {
+    //cenário
+    await request(app)
+      .post("/api/v1/users")
+      .send({ name: "teste", email: "email@email.com.br", password: "senha" });
+
+    //ação
+    const response = await request(app)
+      .post("/api/v1/users")
+      .send({ name: "teste", email: "email@email.com.br", password: "senha" });
+
+    //validação
+    expect(response.status).toBe(400);
+    expect(response.text).toBe('{"message":"User already exists"}');
   });
 });
